f33768c0b7ec94f8e3bde4e25ff371bd96adf579
Lorenz <lorenz.kuhn@bluewin.ch>
2018-05-04 11:38:11 +0200

    initial commit of nn prediction

--- src/run.py
+++ src/run.py
@@ -1,6 +1,10 @@
 import sys
 import time
 import numpy as np
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+import copy
 
 
 # Rows are users
@@ -10,7 +14,7 @@ ITEM_COUNT = 1000
 SUBMISSION_FILE = '../data/submission_sgd.csv'
 SAMPLE_SUBMISSION = '../data/sampleSubmission.csv'
 N_EPOCHS = 20
-LEARNING_RATE = 0.05
+LEARNING_RATE = 0.01
 EPSILON = 0.0001
 
 def load_ratings(data_file):
@@ -58,6 +62,23 @@ def reconstruction_to_predictions(reconstruction):
             predictions.append((i, j, reconstruction[i-1, j-1]))
     write_ratings(predictions)
 
+def get_indices_to_predict():
+    """Get list of indices to predict from sample submission file.
+        Returns:
+            indices_to_predict:  list of tuples with indices"""
+
+    indices_to_predict = []
+    with open(SAMPLE_SUBMISSION, 'r') as file:
+        header = file.readline()
+        for line in file:
+            key, value_string = line.split(",")
+            row_string, col_string = key.split("_")
+            i = int(row_string[1:]) - 1
+            j = int(col_string[1:]) - 1
+            indices_to_predict.append((i, j))
+
+    return indices_to_predict
+
 def write_ratings(predictions):
     with open(SUBMISSION_FILE, 'w') as file:
         file.write('Id,Prediction\n')
@@ -102,8 +123,6 @@ def predict_by_sgd(data, approximation_rank):
             k, l = observed_indices[index]
             u[k, :] += LEARNING_RATE * (data[k, l] - np.dot(u[k, :], z[l, :])) \
                     * z[l, :]
-            #print(LEARNING_RATE * (data[k, l] - np.dot(u[k, :], z[l, :])) \
-                    #* z[l, :])
 
         for j in range(n_samples):
             index = np.random.randint(0, len(observed_indices) - 1)
@@ -124,15 +143,87 @@ def predict_by_sgd(data, approximation_rank):
     return np.dot(u, z.T)
 
 
+def get_embeddings_by_svd(data):
+    """Given data matrix, returns user embeddings and item embeddings."""
+    u, s, vh = np.linalg.svd(data)
+    return u, vh.T
+
+
+def prepare_data_for_nn(user_embeddings, item_embeddings, data_matrix):
+    """Concatenates user embeddings and item embeddings, and adds corresponding rating from data matrix.
+    Returns: x_train, y_train, x_validate, y_validate."""
+
+    x_train = []
+    y_train = []
+
+    x_validate = []
+    y_validate = []
+
+    counter = 0
+    for i, j in zip(*np.nonzero(data_matrix)):
+        x = np.concatenate([user_embeddings[i], item_embeddings[j]])
+        y = data_matrix[i, j]
+        x_train.append(x)
+        y_train.append(y)
+        counter += 1
+        if counter > 1000:
+            break
+        elif counter % 1000 == 0:
+            print(counter)
+
+    x_train = np.asarray(x_train)
+    y_train = np.asarray(y_train)
+    y_train = np.ravel(y_train)
+
+    indices_to_predict = get_indices_to_predict()
+    counter = 0
+    for i, j in indices_to_predict:
+        x = np.concatenate([user_embeddings[i], item_embeddings[j]])
+        y = data_matrix[i, j]
+        x_validate.append(x)
+        y_validate.append(y)
+        counter += 1
+        if counter > 100:
+            break
+        elif counter % 1000 == 0:
+            print(counter)
+
+    x_validate = np.asarray(x_validate)
+    y_validate = np.asarray(y_validate)
+    y_validate = np.ravel(y_validate)
+
+    return x_train, y_train, x_validate, y_validate
+
+
+def predict_by_nn(data_matrix, imputed_data):
+
+    # Get embeddings
+    user_embeddings, item_embeddings = get_embeddings_by_svd(imputed_data)
+
+    x_train, y_train, x_validate, y_validate = prepare_data_for_nn(user_embeddings, item_embeddings, data_matrix)
+    print(y_train)
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.1)
+
+    classifier = MLPClassifier((50, 50))
+
+    classifier.fit(x_train, y_train)
+    accuracy = classifier.score(x_test, y_test)
+    print(accuracy)
+
 
 def main():
     all_ratings = load_ratings('../data/data_train.csv')
     data_matrix = ratings_to_matrix(all_ratings, USER_COUNT, ITEM_COUNT)
     #test_predict_by_avg()
-    #imputed_data = predict_by_avg(data_matrix, True)
+    imputed_data = predict_by_avg(copy.copy(data_matrix), True)
     #reconstruction = predict_by_svd(imputed_data, 2)
-    reconstruction = predict_by_sgd(data_matrix, 10)
-    reconstruction_to_predictions(reconstruction)
+    #reconstruction = predict_by_sgd(data_matrix, 10)
+    #reconstruction_to_predictions(reconstruction)
+
+    predict_by_nn(data_matrix, imputed_data)
+
+
+
 
 if __name__ == '__main__':
     main()
