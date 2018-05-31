Extend ensemble template to compute uniform average. Fix two bugs in load_ratings of utils.

src/ensembler.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import utils
 
+# This file is meant as a template. Feel free to change, replace or copy.
 # TODO(b-hahn): Define and use meaningful naming scheme.
 SUBMISSION_FILE = os.path.join(utils.ROOT_DIR, 'data/ensemble.csv')
 
@@ -9,15 +10,17 @@ def main():
     path = os.path.join(utils.ROOT_DIR, 'data/')
     files = [os.path.join(path, i) for i in os.listdir(path) if \
             os.path.isfile(os.path.join(path,i)) and 'submission_' in i]
-    ratings = []
+    reconstruction = np.zeros((utils.USER_COUNT, utils.ITEM_COUNT),
+            dtype=np.float64)
     for file in files:
+        print('file: ' + str(file))
         file_ratings = utils.load_ratings(file)
         # TODO(b-hahn): Operate on file_ratings in order to manipulate
         # ratings (overall).
-        
-    reconstruction = np.zeros((utils.USER_COUNT, utils.ITEM_COUNT))
-    for row_index, col_index, value in ratings:
-        reconstruction[row_index][col_index] = value
+        for i,j,v in file_ratings:
+            reconstruction[i][j] += v
+    for i, j, v in file_ratings:
+        reconstruction[i][j] = reconstruction[i][j] / len(files)
     utils.reconstruction_to_predictions(reconstruction, SUBMISSION_FILE)
 
 if __name__ == '__main__':

src/utils.py
@@ -23,14 +23,13 @@ def safe_norm(x):
         return np.linalg.norm(x)
 
 def load_ratings(data_file=DATA_FILE):
-    data_file = DATA_FILE
     ratings = []
     with open(data_file, 'r') as file:
         # Read header.
         _ = file.readline()
         for line in file:
             key, value_string = line.split(",")
-            rating = int(value_string)
+            rating = float(value_string)
             row_string, col_string = key.split("_")
             row = int(row_string[1:])
             col = int(col_string[1:])