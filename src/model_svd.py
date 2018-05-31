f5e3f0d070852501a0d9f979ab0dc044ba83ea4e
kkleindev <kleinkevin.lu@gmail.com>
2018-05-31 14:02:56 +0200

    Refactor svd model in order to isolate embedding generation from prediction.

--- src/model_svd.py
+++ src/model_svd.py
@@ -1,6 +1,5 @@
 import os
 import random
-import math
 import numpy as np
 import utils
 
@@ -13,20 +12,21 @@ def write_svd_score(score, k, take_bias):
     with open(SCORE_FILE, 'a+') as file:
         file.write('%d, %s, %f\n' % (k, str(take_bias), score))
 
-def predict_by_svd(data, approximation_rank, take_bias):
-    if take_bias:
-        imputed_data = utils.predict_bias(data)
-    else:
-        imputed_data = utils.predict_by_avg(data, True)
+def get_embeddings(imputed_data, approximation_rank):
+    u_embeddings, singular_values, z_embeddings =\
+            np.linalg.svd(imputed_data)
+    u_embeddings = np.dot(u_embeddings, np.sqrt(singular_values))
+    z_embeddings = np.dot(z_embeddings, np.sqrt(singular_values))
+    u_embeddings = u_embeddings[:, 0:approximation_rank]
+    z_embeddings = z_embeddings[0:approximation_rank, :]
+    return u_embeddings, z_embeddings
+
+def predict_by_svd(data, imputed_data, approximation_rank,):
     reconstruction = imputed_data
     for epoch_index in range(N_EPOCHS):
-        u_embeddings, singular_values, vh_embeddings =\
-                np.linalg.svd(reconstruction)
-        u_embeddings = u_embeddings[:, 0:approximation_rank]
-        singular_values = singular_values[0:approximation_rank]
-        vh_embeddings = vh_embeddings[0:approximation_rank, :]
-        reconstruction = np.dot(u_embeddings,\
-            np.dot(np.diag(singular_values), vh_embeddings))
+        u_embeddings, z_embeddings =\
+                get_embeddings(data, imputed_data, approximation_rank)
+        reconstruction = np.dot(u_embeddings, z_embeddings)
         if epoch_index < N_EPOCHS - 1:
             reconstruction = utils.impute(data, reconstruction)
     return reconstruction
@@ -38,7 +38,11 @@ def main():
     all_ratings = utils.load_ratings()
     data = utils.ratings_to_matrix(all_ratings)
     masked_data = utils.mask_validation(data)
-    reconstruction = predict_by_svd(masked_data, k, take_bias)
+    if take_bias:
+        imputed_data = utils.predict_bias(data)
+    else:
+        imputed_data = utils.predict_by_avg(data, True)
+    reconstruction = predict_by_svd(masked_data, imputed_data, k, take_bias)
     reconstruction = utils.clip(reconstruction)
     rsme = utils.compute_rsme(data, reconstruction)
     print('RSME: %f' % rsme)
