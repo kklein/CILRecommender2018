24f88450b2724e9998cce4a50e759f63ad703a9b
kkleindev <kleinkevin.lu@gmail.com>
2018-06-01 10:23:01 +0200

    Adapt parameters as to reduce execution duration.

--- src/run_chain.py
+++ src/run_chain.py
@@ -7,10 +7,10 @@ SUBMISSION_FILE = os.path.join(utils.ROOT_DIR,\
         'data/chain.csv')
 SCORE_FILE = os.path.join(utils.ROOT_DIR, 'analysis/chain_scores.csv')
 
-N_META_EPOCHS = 6
-N_EPOCHS = 20
+N_META_EPOCHS = 3
+N_EPOCHS = 5
 REGULARIZATION = [0.02, 0.05]
-APPROXIMATION_RANK = 20
+APPROXIMATION_RANK = 10
 
 def main():
     all_ratings = utils.load_ratings()
