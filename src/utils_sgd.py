2d31dc24822b12e60023bca2bca64fff5263261e
kkleindev <kleinkevin.lu@gmail.com>
2018-06-11 14:10:45 +0200

    Use separate arguments for embedding and bias regularization.

--- src/utils_sgd.py
+++ src/utils_sgd.py
@@ -1,9 +1,14 @@
 import numpy as np
 import utils
 
-def write_sgd_score(score, k, regularization, score_file):
+def write_sgd_score(score, k, reg_emb, reg_bias, score_file):
     with open(score_file, 'a+') as file:
-        file.write('%d, %f, %f\n' % (k, regularization, score))
+        file.write('%d, %f, %f, %f\n' % (k, reg_emb, reg_bias, score))
+
+def is_single_regularization(regularization):
+    return isinstance(regularization, np.float64) or\
+            isinstance(regularization, float) or\
+            isinstance(regularization, int)
 
 def get_initialized_biases(data):
     training_indices = utils.get_indeces_from_file(utils.TRAINING_FILE_NAME)
