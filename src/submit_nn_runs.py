912ddfe4a01eb41060402eabc5e3670c76406506
Lorenz <lorenz.kuhn@bluewin.ch>
2018-06-14 10:28:02 +0200

    refactor model_nn.py

--- src/submit_nn_runs.py
+++ src/submit_nn_runs.py
@@ -3,5 +3,5 @@ import subprocess
 for embedding_dimensions in range(10,200,10):
     for alpha in [0.0001, 0.001, 0.01]:
         hidden_layer_width = int(embedding_dimensions / 2)
-        args = ["bsub", "-R", "rusage[mem=6000]", "python", "nn.py", "svd", str(embedding_dimensions), "("+str(hidden_layer_width)+",)", "1000000", str(alpha)]
+        args = ["bsub", "-R", "rusage[mem=6000]", "python", "model_nn.py", "svd", str(embedding_dimensions), "("+str(hidden_layer_width)+",)", "1000000", str(alpha)]
         subprocess.Popen(args)
