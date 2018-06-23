2957de50ff131b957777792e6c92aead71ce1660
kkleindev <kleinkevin.lu@gmail.com>
2018-06-23 10:15:50 +0200

    Update run_chain to changes in client methods.

--- src/run_chain.py
+++ src/run_chain.py
@@ -10,33 +10,31 @@ SCORE_FILE = os.path.join(utils.ROOT_DIR, 'analysis/chain_scores.csv')
 
 N_META_EPOCHS = 3
 N_EPOCHS = 2
-REGULARIZATION = [0.02, 0.05]
+REG_EMB = 0.02
+REG_BIAS = 0.05
 APPROXIMATION_RANK = 4
 
 def main():
     all_ratings = utils.load_ratings()
     data = utils.ratings_to_matrix(all_ratings)
     masked_data = utils.mask_validation(data)
-    imputed_data = utils.novel_init(masked_data)
+    reconstruction = np.copy(masked_data)
+    utils.impute_by_novel(reconstruction)
     print('Initial imputation completed.')
-    if np.isnan(imputed_data).any():
-        raise ValueError('Matrix initialization contains NaNs.')
     for _ in range(N_META_EPOCHS):
         print("Computing embeddings.")
         u_embeddings, z_embeddings =\
-                svd.get_embeddings(imputed_data, APPROXIMATION_RANK)
+                svd.get_embeddings(reconstruction, APPROXIMATION_RANK)
         if np.isnan(u_embeddings).any() or np.isnan(z_embeddings).any():
             raise ValueError('Embeddings contain NaNs.')
         print("Executing sgd by sf.")
         reconstruction = sf.predict_by_sf(masked_data,
-                regularization=REGULARIZATION, n_epochs=N_EPOCHS,
+                reg_emb=REG_EMB, reg_bias=REG_BIAS, n_epochs=N_EPOCHS,
                 u_embedding=u_embeddings, z_embedding=z_embeddings)
         if np.isnan(reconstruction).any():
             raise ValueError('Sf reconstruction created NaNs.')
-        # TODO(kkleindev): Figure out whether to use reconstruction or
-        # reconstruction used as imputation
-        imputed_data = utils.impute(masked_data, reconstruction)
-    rsme = utils.compute_rsme(data, imputed_data)
+        utils.ampute_reconstruction(reconstruction, data)
+    rsme = utils.compute_rsme(data, reconstruction)
     print(rsme)
     # write_chain_score(SCORE_FILE)
     utils.reconstruction_to_predictions(reconstruction, SUBMISSION_FILE)
