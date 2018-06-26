b6d390a67672322d60a3f57aea0a78783d9e43b3
heylook <kuhnl@eu-login-01.euler.ethz.ch>
2018-06-26 15:54:02 +0200

    fix tex. update grid search results

--- src/submit_nn_runs.py
+++ src/submit_nn_runs.py
@@ -1,9 +1,9 @@
 import subprocess
 
 for embedding_dimensions in range(10,200,10):
-    for embedding_type in ["iterated_svd", "svd", "ids", "nmf", "lle", "pca"]:
+    for embedding_type in ["sf", "reg_sgd"]:
         hidden_layer_width = int(embedding_dimensions / 2)
         for architecture in [(int(embedding_dimensions / 2),), (int(embedding_dimensions / 2), int(embedding_dimensions / 4)), (int(embedding_dimensions / 2),int(embedding_dimensions / 4),int(embedding_dimensions / 8))]:
 
-            args = ["bsub", "-R", "rusage[mem=6000]", "python", "model_nn.py", embedding_type, str(embedding_dimensions), str(architecture), "1500000","0.0001"]
+            args = ["bsub", "-W", "20:00", "-R", "rusage[mem=6000]", "python", "model_nn.py", embedding_type, str(embedding_dimensions), str(architecture), "1500000","0.0001"]
             subprocess.Popen(args)
